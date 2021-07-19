89fe1ff8516e2923d344da9c8cfdcc168cdbedc6
Irek81 <irekisanbirdin@gmail.com>
2021-07-19 17:03:35 +0500

    main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        int amount1 = 2_000_000_000;
+        int amount2 = 500_000_000;
+        int total;
+        total = amount1 + amount2;
+
+        System.out.println(total);
+    }
 }
